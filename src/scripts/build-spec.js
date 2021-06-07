ee1106441f5eedfef52da55dc8663fa5208e2988
Grugknuckle <awol@bete.com>
2021-06-07 16:27:49 -0400

    build openapi spec json + yaml

--- src/scripts/build-spec.js
+++ src/scripts/build-spec.js
@@ -7,7 +7,7 @@ const yaml = require('json-to-pretty-yaml')
 const fs = require('fs')
 const path = require('path')
 
-const directory = path.join(__dirname, './../api-specs')
+const directory = path.join(__dirname, './../openapi-specs')
 
 buildAPISpec(directory)
 
