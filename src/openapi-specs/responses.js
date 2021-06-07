c1a20d330de402ddfdc7290c9609a6e6cb2c1208
Grugknuckle <awol@bete.com>
2021-06-07 16:26:40 -0400

    trimmed response json

--- src/openapi-specs/responses.js
+++ src/openapi-specs/responses.js
@@ -1,70 +1,14 @@
 
 module.exports = {
-  get(schema) {
-    return {
-      '200': { description: 'OK', content: content(schema) },
-      '400': { '$ref': '#/components/responses/400' },
-      '401': { '$ref': '#/components/responses/401' },
-      '403': { '$ref': '#/components/responses/403' },
-      '404': { '$ref': '#/components/responses/404' },
-      '500': { '$ref': '#/components/responses/500' }
-    }
-  },
-  put(schema) {
-    return {
-      '200': { description: 'OK', content: content(schema) },
-      '304': { '$ref': '#/components/responses/304' },
-      '400': { '$ref': '#/components/responses/400' },
-      '401': { '$ref': '#/components/responses/401' },
-      '403': { '$ref': '#/components/responses/403' },
-      '404': { '$ref': '#/components/responses/404' },
-      '500': { '$ref': '#/components/responses/500' }
-    }
-  },
-  post(schema) {
-    return {
-      '201': { description: 'Created', content: content(schema) },
-      '400': { '$ref': '#/components/responses/400' },
-      '401': { '$ref': '#/components/responses/401' },
-      '403': { '$ref': '#/components/responses/403' },
-      '404': { '$ref': '#/components/responses/404' },
-      '500': { '$ref': '#/components/responses/500' }
-    }
-  },
-  delete(schema) {
-    return {
-      '200': { description: 'OK', content: content(schema) },
-      '304': { '$ref': '#/components/responses/304' },
-      '400': { '$ref': '#/components/responses/400' },
-      '401': { '$ref': '#/components/responses/401' },
-      '403': { '$ref': '#/components/responses/403' },
-      '404': { '$ref': '#/components/responses/404' },
-      '500': { '$ref': '#/components/responses/500' }
-    }
-  },
-  notImplemented() {
-    return {
-      '501': { '$ref': '#/components/responses/501' }
-    }
-  },
-  all() {
-    return {
-      '200': { description: 'OK', content: content() },
-      '201': { description: 'CREATED', content: content() },
-      '204': { description: 'NO CONTENT', content: content() },
-      '304': { description: 'NOT MODIFIED', content: content() },
-      '400': { description: 'BAD REQUEST', content: content() },
-      '401': { description: 'UNAUTHORIZED', content: content() },
-      '403': { description: 'FORBIDDEN', content: content() },
-      '404': { description: 'NOT FOUND', content: content() },
-      '500': { description: 'SERVER ERROR', content: content({ '$ref': '#/components/schemas/netsuiteError' }) },
-      '501': { description: 'NOT IMPLEMENTED', content: content() },
-    }
-  },
   content,
   xlsx
 }
 
+/**
+ * 
+ * @param {*} schema 
+ * @returns 
+ */
 function content(schema) {
   // schema: { '$ref': '#/components/schemas/response' }
   data = (!!schema) ? schema : { type: 'object', description: 'An object containing the successful response data.' }
@@ -87,6 +31,11 @@ function content(schema) {
   }
 }
 
+/**
+ * Defines the schema for returning an Excel spreadsheet.
+ * 
+ * @returns {object} The schema of an xlsx spreadsheet to be returned.
+ */
 function xlsx() {
   return {
     'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet': {
