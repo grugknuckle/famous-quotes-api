4a03b7578ee367dbcfe08e696f6e38bd07c9a5c2
grugknuckle <aaron.wolbach@gmail.com>
2021-06-12 13:35:13 -0400

    add '/' to issuer

--- server/app.js
+++ server/app.js
@@ -41,7 +41,7 @@ const verifyJWT = jwt({
     jwksUri: `${process.env.AUTH0_ISSUER_BASE_URL}/.well-known/jwks.json`
   }),
   audience: process.env.AUTH0_AUDIENCE,
-  issuer: process.env.AUTH0_ISSUER_BASE_URL,
+  issuer: `${process.env.AUTH0_ISSUER_BASE_URL}/`,
   algorithms: ['RS256']
 })
 
