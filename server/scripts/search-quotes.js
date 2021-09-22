69f810a1b6adcc9ac0747ff8ad587fdaa4c4e2f0
Aaron Wolbach <aaron.wolbach@auth0.com>
2021-09-22 18:12:33 -0400

    update tf-idf indexer

--- server/scripts/search-quotes.js
+++ server/scripts/search-quotes.js
@@ -1,11 +1,9 @@
 const path = require('path')
 require('dotenv').config({ path: path.join(__dirname, '../../.env.prod') })
-const jsonfile = require('jsonfile')
+const Indexer = require('./../lib/TfIdf')
 const Database = require('../lib/Database')
 const QuoteModel = require('../models/quote.model')
 const AuthorModel = require('../models/author.model')
-
-const natural = require('natural')
 
 const args = process.argv.slice(2)
 const searchString = args.length ? args[0] : 'war fighter'
@@ -13,18 +11,26 @@ const searchString = args.length ? args[0] : 'war fighter'
 main()
 
 async function main() {
-  console.log(`searching for "${searchString}"."`)
+  
   try {
     const database = new Database(process.env.DB_CONNECTION_STRING)
     await database.connect()
 
-    const results = []
-    let found = await rankQuotes()
+    const quotes = await getAllQuotes()
+    const indexer = new Indexer(quotes.docs)
+
+    console.log(`searching for "${searchString}".`)
+    console.log(`search string stems: ${indexer.tokenizeAndStem(searchString)}`)
+
+    const results = indexer.rankDocuments(searchString)
+
+    // const results = []
+    // let found = await rankQuotes()
     
-    for (const item of found) {
-      const quote = await findQuoteByID(item.id)
-      results.push(Object.assign(item, { text: quote.text, author: quote.author.fullName }))
-    }
+    // for (const item of found) {
+    //   const quote = await findQuoteByID(item.id)
+    //   results.push(Object.assign(item, { text: quote.text, author: quote.author.fullName }))
+    // }
 
     console.log(results)
     await database.disconnect()
@@ -43,7 +49,13 @@ async function rankQuotes() {
   const tfidf = new TfIdf(index.tfidf)
 
   const results = []
-  tfidf.tfidfs(tokenizer.tokenize(searchString), (i, measure) => {
+  // tokenize and stem the search string (ENGLISH ONLY !)
+  const stems = tokenizer
+    .tokenize(searchString)
+    .map(token => natural.LancasterStemmer.stem(token))
+
+  // compute the measures
+  tfidf.tfidfs(stems, (i, measure) => {
     if (measure > 0) {
       const id = index.lookup[i]
       const result = { measure, id }
@@ -51,6 +63,19 @@ async function rankQuotes() {
     }
   })
   return results.sort((a, b) => b.measure - a.measure)
+}
+
+async function getAllQuotes() {
+  const query = {
+    page: 1,
+    limit: 250,
+    sort: 'author'
+  }
+
+  const { filter, options } = QuoteModel.parseQuery(query)
+  const found = await QuoteModel.paginate(filter, options)
+  found.docs = found.docs.map(document => document.format())
+  return found
 }
 
 async function findQuoteByID(id) {
--- server/scripts/tfidf-indexer.js
+++ server/scripts/tfidf-indexer.js
@@ -1,11 +1,9 @@
 const path = require('path')
 require('dotenv').config({ path: path.join(__dirname, '../../.env.prod') })
-const jsonfile = require('jsonfile')
 const Database = require('../lib/Database')
 const QuoteModel = require('../models/quote.model')
+const Indexer = require('./../lib/TfIdf')
 
-const natural = require('natural')
-const sw = require('stopword')
 main()
 
 async function main() {
@@ -16,15 +14,10 @@ async function main() {
     const quotes = await getAllQuotes()
     await database.disconnect()
 
-    // tokenize quote texts, remove stopwords, add documents to TfIdf corpus
-    const corpus = quotes.docs.map(doc => doc.text)
-    const lookup = quotes.docs.map(doc => doc.id)
-    const tfidf = await indexDocuments(corpus)
-
-    // serialize TfIdf to JSON, save it with a quote-id lookup table.
+    // console.log(quotes)
+    const indexer = new Indexer(quotes.docs)
     const filename = path.join(__dirname, './../backup/TfIdf.json')
-    const data = { lookup, tfidf }
-    const response = await jsonfile.writeFile(filename, data, { spaces: 2 })
+    const response = await indexer.saveToFile(filename)
 
     console.log('DONE !')
   } catch (error) {
@@ -44,17 +37,3 @@ async function getAllQuotes() {
   found.docs = found.docs.map(document => document.format())
   return found
 }
-
-async function indexDocuments(corpus) {
-  const TfIdf = natural.TfIdf
-  const tfidf = new TfIdf()
-  const tokenizer = new natural.WordTokenizer()
-
-  for(const text of corpus) {
-    // tokenize quote text and remove stopwords
-    const document = sw.removeStopwords(tokenizer.tokenize(text))
-    tfidf.addDocument(document)
-  }
-  return tfidf
-}
-
